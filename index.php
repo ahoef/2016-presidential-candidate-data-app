110a5313728a4212deb1e5eda76eafea954ca5ce
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2015-11-03 23:20:53 -0500

    set up basic layout and colors

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 	  	<link rel="icon" type="image/png" href="http://i.imgur.com/bsI3IOt.png">
 	 	<link rel="stylesheet" href="styles/dest/base.min.css">
-	  	<link href='//fonts.googleapis.com/css?family=Lato:300,400|Oswald:400,300' rel='stylesheet' type='text/css'>
+	  	<link href='https://fonts.googleapis.com/css?family=Raleway:400,800|Lato' rel='stylesheet' type='text/css'>
 
 	  	<?php require("includes/connect.php"); ?>
 
@@ -23,10 +23,10 @@
 	<body>
 		<header>
 			<h1>Hear it from them!</h1>
-			<h2>Quotes from 2016 Presidential Candidates</h2>
+			<h2>Words from 2016 Presidential Candidates</h2>
 		</header>
 
-		<section class="add-quote">
+<!-- 		<section class="add-quote">
 			<h3>Add a quote that you've heard or read! </h3>
 			<form method='POST' action='index.php' id='new-quote-form'>
 				<label><strong>Who</strong> said it?</label>
@@ -51,20 +51,19 @@
 				<input type="text" name="date" required ></input>
 
 				<input type="submit" value="Submit"></input>
-			</form>
+			</form> 
 
 
-			<?php print_r($_POST); ?>
+			<?php //print_r($_POST); ?>
 
-		</section>
+		</section>-->
 
 		<section class="content-container">
 			<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-2">
 
 				<li class="candidate">
 					<img src="images/cropped/hillary-clinton.jpg" alt="Hillary Clinton" >
-					<h3>Hillary Clinton</h3>
-					<h4>Democrat</h4>
+					<h3>Hillary Clinton</h3>, <h4>Democrat</h4>
 					<h5>Former U.S. Secretary of State, Former Senator from New York</h5>
 					<ul>
 						<?php
@@ -76,8 +75,7 @@
 
 				<li class="candidate">
 					<img src="images/cropped/ben-carson.jpg" alt="Ben Carson" >
-					<h3>Ben Carson</h3>
-					<h4>Republican</h4>
+					<h3>Ben Carson</h3>, <h4>Republican</h4>
 					<h5>Former Director of Pediatric Neurosurgery at Johns Hopkins Hospital</h5>
 					<ul>
 						<?php
@@ -89,8 +87,7 @@
 
 				<li class="candidate">
 					<img src="images/cropped/bernie-sanders.jpg" alt="Bernie Sanders" >
-					<h3>Bernie Sanders</h3>
-					<h4>Democrat</h4>
+					<h3>Bernie Sanders</h3>, <h4>Democrat</h4>
 					<h5>Senator from Vermont, Former member of the Vermont House of Representatives</h5>
 					<ul>
 						<?php
@@ -102,8 +99,7 @@
 
 				<li class="candidate">
 					<img src="images/cropped/donald-trump.jpg" alt="Donald Trump" >
-					<h3>Donald Trump</h3>
-					<h4>Republican</h4>
+					<h3>Donald Trump</h3>, <h4>Republican</h4>
 					<h5>Chairman and President of The Trump Organization, Chairman of Trump Plaza Associates, LLC, Chairman of Trump Atlantic City Associates, Host of NBC reality show 'The Apprentice'</h5>
 					<ul>
 						<?php
@@ -115,8 +111,7 @@
 
 				<li class="candidate">
 					<img src="images/cropped/martin-omalley.jpg" alt="Martin O'Malley" >
-					<h3>Martin O'Malley</h3>
-					<h4>Democrat</h4>
+					<h3>Martin O'Malley</h3>, <h4>Democrat</h4>
 					<h5>Former Governor of Maryland</h5>
 					<ul>
 						<?php
@@ -128,8 +123,7 @@
 
 				<li class="candidate">
 					<img src="images/cropped/marco-rubio.jpg" alt="Marco Rubio" >
-					<h3>Marco Rubio</h3>
-					<h4>Republican</h4>
+					<h3>Marco Rubio</h3>, <h4>Republican</h4>
 					<h5>Senator from Florida, Former Speaker of the Florida House of Representatives</h5>
 					<ul>
 						<?php
@@ -141,8 +135,7 @@
 
 				<li class="candidate">
 					<img src="images/cropped/lawrence-lessig.jpg" alt="Lawrence Lessig" >
-					<h3>Lawrence Lessig</h3>
-					<h4>Democrat</h4>
+					<h3>Lawrence Lessig</h3>, <h4>Democrat</h4>
 					<h5>Professor of Law at Harvard Law School, Founder of Creative Commons</h5>
 					<ul>
 						<?php
@@ -154,8 +147,7 @@
 
 				<li class="candidate">
 					<img src="images/cropped/jeb-bush.jpg" alt="Jeb Bush" >
-					<h3>Jeb Bush</h3>
-					<h4>Republican</h4>
+					<h3>Jeb Bush</h3>, <h4>Republican</h4>
 					<h5>Governor of Florida</h5>
 					<ul>
 						<?php
@@ -175,15 +167,20 @@
 
 	<footer>
 
-		<p>Visit these links to view the most up-to-date poll results for each party:</p>
+		<!-- <p>Visit these links to view the most up-to-date poll results for each party:</p> -->
 		<a href="http://elections.huffingtonpost.com/pollster/2016-national-democratic-primary">
-			2016 National Democratic Primary
+			2016 National Democratic Primary Polls
 		</a>
 		<a href="http://elections.huffingtonpost.com/pollster/2016-national-gop-primary">
-			2016 National GOP Primary
+			2016 National GOP Primary Polls
 		</a>
 
-		<p>This project was built by Alexandra Hoefinger with php, MySQL, GulpJS, and Sass. <a href="https://github.com/ahoef/2016-presidential-candidate-data-app" title="View this project on github">View on github</a></p>
+		<aside>This project was built by Alexandra Hoefinger with PHP, MySQL, GulpJS, and Sass. <a href="https://github.com/ahoef/2016-presidential-candidate-data-app" title="View this project on github">View on github</a></aside>
+		  <ul class="social-links">
+            <li><a href="https://github.com/ahoef" target="_blank"><i class="icon-github"></i></a></li>
+            <li><a href="https://twitter.com/ahoefinger" target="_blank"><i class="icon-twitter"></i></a></li>
+            <li><a href="mailto:alexandra.hoefinger@gmail.com"><i class="icon-mail"></i></a></li>
+          </ul>
 		
 	</footer>
 </html>
